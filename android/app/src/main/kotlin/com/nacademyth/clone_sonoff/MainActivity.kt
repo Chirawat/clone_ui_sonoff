1f420401c93d8ab8063e4f9d39ef43ab70ce264d
Chirawat <chirawat.not@gmail.com>
2021-11-14 17:14:24 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/nacademyth/clone_sonoff/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nacademyth.clone_sonoff
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextStyle titleStyle = TextStyle(
+    color: Color(0xFF5B777B),
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Color(0xff5641d5),
+              Color(0xff3d93ff),
+            ])),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+          child: Column(
+            children: [
+              Device(),
+              Device(),
+              Device(),
+              Device(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.yellow,
+        backgroundColor: Color(0xff3d93ff),
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.view_list),
+              label: 'All Devices',
+              backgroundColor: Colors.greenAccent),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add Device',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Account',
+            //backgroundColor: Colors.purple,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Device extends StatelessWidget {
+  const Device({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0),
+      width: double.infinity,
+      //height: MediaQuery.of(context).padding.bottom,
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Color(0xffe5e8fb),
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            //padding: const EdgeInsets.all(8.0),
+            child: Row(children: [
+              Container(
+                width: 50,
+                height: 50,
+                //color: Colors.blue,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0x7F000000),
+                ),
+                child: Icon(Icons.power_settings_new),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text(
+                'Front Light',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 22.0),
+              ),
+            ]),
+          ),
+          Icon(Icons.wifi, color: Colors.blue,)
+        ],
+      ),
+    );
+  }
+}
